cd42c25549f8be39a5b1eadd36e59a11a95f8b1e
khan.sadirac42 <khan.sadirac42@gmail.com>
2024-11-27 17:49:57 +0000

    init hexagonal architecture for the project with scoring feature

--- repository/score.go
+++ repository/score.go
@@ -2,9 +2,29 @@ package repository
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/ficoback/domain"
 )
+
+var mockScores = map[string]BankAccount{
+	"1": {
+		Income:  1000,
+		Outcome: 500,
+	},
+	"2": {
+		Income:  2000,
+		Outcome: 1000,
+	},
+	"toto": {
+		Income:  3000,
+		Outcome: 5000,
+	},
+	"picsou": {
+		Income:  10000,
+		Outcome: 0,
+	},
+}
 
 type ScoreRepository struct{}
 
@@ -13,9 +33,21 @@ func NewRepositoryScore() ScoreRepository {
 }
 
 func (r ScoreRepository) GetScore(_ context.Context, ID string) (domain.Score, error) {
+	bankAccount, ok := mockScores[ID]
+	if !ok {
+		return domain.Score{}, fmt.Errorf("current ID doesn't have any score")
+	}
+
+	financialScore := calculFinancialScore(bankAccount)
+
 	score := domain.Score{
 		ID:    ID,
-		Score: 100,
+		Score: financialScore,
 	}
+
 	return score, nil
+}
+
+func calculFinancialScore(bankAccount BankAccount) int {
+	return bankAccount.Income - bankAccount.Outcome
 }
---
+++ repository/stst.go
@@ -0,0 +1,6 @@
+package repository
+
+type BankAccount struct {
+	Income  int
+	Outcome int
+}
